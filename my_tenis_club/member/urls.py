b040f1fe6232e1640e51b5552d9b804922d3769b
Dheeraj2002kumar <dk8549644@gmail.com>
2026-05-07 17:59:32 +0530

    Add Main index page to someone visits the root folder of the project

--- my_tenis_club/member/urls.py
+++ my_tenis_club/member/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path('members/', views.member_list, name='member_list'),
     path('members/details/<int:id>', views.details, name='details'),
 ]
